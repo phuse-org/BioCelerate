591d94fe933b99e4d16132e5ba289b0579eaf1f6
Daniel P. Russo <russodanielp@gmail.com>
2020-07-23 14:17:03 -0400

    wrote a config.R script with a CLI to make the syspaters. R file

---
+++ config.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+library(tools)
+require(stringi)
+
+askPrompt <- "Please enter the folder containing the SEND datasets: "
+studyRoot <- file.path(readline(prompt=askPrompt))
+
+while (!(file.exists(studyRoot))) {
+  
+  checkPrompt <- paste(sprintf("Cannot find folder: %s\n", studyRoot), askPrompt)
+  studyRoot <- file.path(readline(prompt=checkPrompt))
+}
+
+askPrompt <- "Please enter the folder where you would like to store resulting SEND SQLite database: "
+dbFolder <- file.path(readline(prompt=askPrompt))
+
+while (!(file.exists(dbFolder))) {
+  
+  checkPrompt <- paste(sprintf("Cannot find folder: %s\n", dbFolder), askPrompt)
+  dbFolder <- file.path(readline(prompt=checkPrompt))
+}
+
+scriptContents <- sprintf("# sysParameters.R created by config.R
+
+library(tools)
+
+
+studyRoot <- file.path('%s')
+metadataRoot <- file.path(file_path_as_absolute('.'), 'metadata')
+
+# SEND data base name and location
+dbFullName <- file.path('%s', 'send.db')
+
+", stri_escape_unicode(studyRoot), stri_escape_unicode(dbFolder))
+
+
+
+fileConn <- file(file.path(file_path_as_absolute("."), 'sysParameters.R'))
+writeLines(c(scriptContents), fileConn)
+close(fileConn)
